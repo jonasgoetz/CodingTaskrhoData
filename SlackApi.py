11e5bbebcf7fa75f72c94fbae4eb585994abe564
Jonas Götz <jg133@hdm-stuttgart.de>
2020-07-04 00:17:13 +0200

    changed script to run endlessly, added error handling

--- SlackApi.py
+++ SlackApi.py
@@ -1,7 +1,10 @@
 import csv
 import json
 import os
+import traceback
 import urllib.request
+from datetime import datetime
+from time import sleep
 
 from dotenv import load_dotenv
 
@@ -10,27 +13,35 @@ def json_request(method, token, channel, thread_ts):
     """"requesting a given method from the slack api and returning a
     dictionary
     """
-    json_link = str("https://slack.com/api/conversations."
-                    "{0:s}?token={1:s}&channel={2:s}{3:s}&pretty=1"
-                    .format(method, token, channel, thread_ts))
-    print("Requesting conversations.{0:s}".format(method), json_link)
-    url_request = urllib.request.urlopen(json_link)
-    data = url_request.read()
-    encoding = url_request.info().get_content_charset('utf-8')
-    json_data = json.loads(data.decode(encoding))
-    return json_data
+    try:
+        json_link = str("https://slack.com/api/conversations."
+                        "{0:s}?token={1:s}&channel={2:s}{3:s}&pretty=1"
+                        .format(method, token, channel, thread_ts))
+        print("Requesting conversations.{0:s}".format(method), json_link)
+        url_request = urllib.request.urlopen(json_link)
+        data = url_request.read()
+        encoding = url_request.info().get_content_charset('utf-8')
+        json_data = json.loads(data.decode(encoding))
+        return json_data
+
+    except Exception as e:
+        print(str(datetime.now()) + ": " + str(e))
 
 
 def get_all_channels():
     """retrieving all channels, saving channel id and name in a dict"""
-    json_list = json_request("list", token, "", "")
-    number_of_channels = len(json_list["channels"])
-    channels_in_workspace = {}
-    for i in range(number_of_channels):
-        channel_name = json_list["channels"][i]["name"]
-        channel_id = json_list["channels"][i]["id"]
-        channels_in_workspace[channel_name] = channel_id
-    return channels_in_workspace
+    try:
+        json_list = json_request("list", token, "", "")
+        number_of_channels = len(json_list["channels"])
+        channels_in_workspace = {}
+        for i in range(number_of_channels):
+            channel_name = json_list["channels"][i]["name"]
+            channel_id = json_list["channels"][i]["id"]
+            channels_in_workspace[channel_name] = channel_id
+        return channels_in_workspace
+
+    except:
+        print("frali", traceback.format_exc())
 
 
 def generate_csv_header():
@@ -74,47 +85,60 @@ def retrieve_all_messages(csv_columns):
         "thread_ts",
     )
     for key in channels_in_workspace:
-
         channel = channels_in_workspace[key]
         single_message = {}
-        json_history = json_request("history", token, channel, "")
-        l = (len(json_history["messages"]))
-        for i in range(l):
-            # iterating through keys of every message
-            for key, value in json_history["messages"][i].items():
-                if key in data_to_retrieve:
-                    if key == "thread_ts":
-                        # checks if a "thread_ts" exists
-                        single_message["thread_ts"] = value
-                        single_message["is_parent_message"] = True
+        try:
+            json_history = json_request("history", token, channel, "")
+            l = (len(json_history["messages"]))
+            for i in range(l):
+                # iterating through keys of every message
+                for key, value in json_history["messages"][i].items():
+                    if key in data_to_retrieve:
+                        if key == "thread_ts":
+                            # checks if a "thread_ts" exists
+                            single_message["thread_ts"] = value
+                            single_message["is_parent_message"] = True
 
-                        # retrieving the replies
-                        message_reply = {}
-                        json_replies = json_request(
-                            "replies", token, channel, "&ts=" + value)
-                        l = (len(json_replies["messages"]))
-                        # range starting from 1 to skip the first element
-                        # because its the parent
-                        for i in range(1, l):
-                            for key, value in \
-                                    json_replies["messages"][i].items():
-                                if key in data_to_retrieve:
-                                    message_reply[key] = value
-                            message_reply["is_child_message"] = True
-                            message_reply["channelId"] = channel
-                            dict_to_csv(message_reply, csv_columns)
-                    else:
-                        single_message[key] = value
-                        single_message["thread_ts"] = None
-                        single_message["is_parent_message"] = None
-            single_message["channelId"] = channel
-            dict_to_csv(single_message, csv_columns)
+                            # retrieving the replies
+                            message_reply = {}
+                            try:
+                                json_replies = json_request(
+                                    "replies", token, channel, "&ts=" + value)
+                                l = (len(json_replies["messages"]))
+                                """range starting from 1 to skip the first 
+                                element because its the parent
+                                """
+                                for i in range(1, l):
+                                    for key, value in \
+                                            json_replies["messages"][
+                                                i].items():
+                                        if key in data_to_retrieve:
+                                            message_reply[key] = value
+                                    message_reply["is_child_message"] = True
+                                    message_reply["channelId"] = channel
+                                    dict_to_csv(message_reply, csv_columns)
+
+                            except:
+                                print(traceback.format_exc())
+
+                        else:
+                            single_message[key] = value
+                            single_message["thread_ts"] = None
+                            single_message["is_parent_message"] = None
+                single_message["channelId"] = channel
+                dict_to_csv(single_message, csv_columns)
+
+        except:
+            print(traceback.format_exc())
 
 
 if __name__ == "__main__":
     load_dotenv()
     token = os.getenv("SLACK_API_TOKEN")
-    get_all_channels()
-    csv_columns = generate_csv_header()
-    channels_in_workspace = get_all_channels()
-    retrieve_all_messages(csv_columns)
+    while True:
+        get_all_channels()
+        csv_columns = generate_csv_header()
+        channels_in_workspace = get_all_channels()
+        retrieve_all_messages(csv_columns)
+        # slowing the script down to stay under 20 requests per minute
+        sleep(1)
